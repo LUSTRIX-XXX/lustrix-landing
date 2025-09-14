e65307ce76369c60cf00d408281e4473e9d20138
LUSTRIX-XXX <raulsb221@gmail.com>
2025-09-14 02:21:46 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
